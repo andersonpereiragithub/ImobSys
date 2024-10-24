ff9a1544571e93ffd772dd06618c1d0aab6835a5
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-10-23 22:25:06 -0300

    Implementado ImoveisRepository e JsonImovelRepository

---
+++ Infrastructure/Repositories/IImoveisRepository.cs
@@ -0,0 +1,12 @@
+﻿using ImobSys.Domain;
+
+namespace ImobSys.Infrastructure.Repositories
+{
+    public interface IImoveisRepository
+    {
+        void Salvar(Imovel imovel);
+        Imovel BuscarPorId(Guid id);
+        List<Imovel> ListarTodos();
+        void Remover(Guid id);
+    }
+}
---
+++ Infrastructure/Repositories/JsonImovelRpository.cs
@@ -0,0 +1,74 @@
+﻿using ImobSys.Domain;
+using Newtonsoft.Json;
+
+namespace ImobSys.Infrastructure.Repositories
+{
+    internal class JsonImovelRpository : IImoveisRepository
+    {
+        private readonly string _filePath;
+
+        public JsonImovelRpository()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            _filePath = Path.Combine(projectDirectory, "Infrastructure", "Persistence", "Data", "imoveis.json");
+        }
+        public void Salvar(Imovel imovel)
+        {
+            var imoveis = ListarTodos();
+            var existente = imoveis.Find(i => i.Id == imovel.Id);
+
+            if (existente != null)
+            {
+                imoveis.Remove(existente);
+            }
+
+            imoveis.Add(imovel);
+
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(imoveis));
+        }
+
+        public Imovel BuscarPorId(Guid id)
+        {
+            var imoveis = ListarTodos();
+            var imovel = imoveis.Find(imovel => imovel.Id == id);
+
+            if (imovel == null)
+            {
+                throw new Exception("Imóvel não encontrado.");
+            }
+
+            return imovel;
+        }
+
+        public List<Imovel> ListarTodos()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Imovel>();
+            }
+
+            var json = File.ReadAllText(_filePath);
+            var imoveis = JsonConvert.DeserializeObject<List<Imovel>>(json);
+
+            if (imoveis == null)
+            {
+                return new List<Imovel>();
+            }
+
+            return imoveis;
+        }
+
+        public void Remover(Guid id)
+        {
+            var imoveis = ListarTodos();
+            var imovel = imoveis.Find(i => i.Id == id);
+
+            if (imovel != null)
+            {
+                imoveis.Remove(imovel);
+                File.WriteAllText(_filePath, JsonConvert.SerializeObject(imoveis, Formatting.Indented));
+            }
+        }
+
+    }
+}
