6e103316cf4e4a4e1a0d8b1040455e5b124bf76f
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-12-05 00:01:31 -0300

    Refatora ImovelService e UserInteractionHandler.

--- Application/Services/ImovelService.cs
+++ Application/Services/ImovelService.cs
@@ -25,16 +25,17 @@ namespace ImobSys.Application.Services
         {
             var novoImovel = new Imovel { Id = Guid.NewGuid() };
 
-            novoImovel.TipoImovel = ConfigurarTipoImovel();
-            novoImovel.AreaUtil = ObterAreaUtil();
-            novoImovel.InscricaoIPTU = SolicitarCampo("Inscrição de IPTU:", false);
-            novoImovel.DetalhesTipoImovel = ObterTipoImovel();
+            novoImovel.TipoImovel = _userInteractionHandler.ConfigurarTipoImovel();
+            novoImovel.AreaUtil = _userInteractionHandler.ObterAreaUtil();
+            
+            novoImovel.InscricaoIPTU = _userInteractionHandler.SolicitarCampo("Inscrição de IPTU:", false);
+            novoImovel.DetalhesTipoImovel = _userInteractionHandler.ObterTipoImovel();
 
-            ConfigurarLocacaoEVenda(novoImovel);
+            _userInteractionHandler.ConfigurarLocacaoEVenda(novoImovel);
 
-            novoImovel.Endereco = ObterEndereco();
+            novoImovel.Endereco = _userInteractionHandler.ObterEndereco();
 
-            ConfigurarCaracteristicasInternas(novoImovel);
+            _userInteractionHandler.ConfigurarCaracteristicasInternas(novoImovel);
 
             AtribuirProprietarios(novoImovel);
 
@@ -44,141 +45,6 @@ namespace ImobSys.Application.Services
 
             _userInteractionHandler.ExibirMensagem("Pressione qualquer tecla para retornar ao Menu.");
             Console.ReadKey();
-        }
-
-        private string ConfigurarTipoImovel()
-        {
-            string tipoImovel = "";
-            do
-            {
-                _userInteractionHandler.ExibirMensagem("Tipo do Imóvel \n    (1)Residencial\n    (2)Comercial\n    (3)Misto)\n          Opção: ");
-                if (int.TryParse(Console.ReadLine(), out int escolha))
-                {
-                    tipoImovel = escolha switch
-                    {
-                        1 => "Residencial",
-                        2 => "Comercial",
-                        3 => "Misto",
-                        _ => tipoImovel
-                    };
-                    if (!string.IsNullOrEmpty(tipoImovel)) break;
-                }
-                Console.WriteLine("Opção Inválida! Escolha 1, 2 ou 3.");
-            } while (true);
-            return tipoImovel;
-        }
-
-        private float ObterAreaUtil()
-        {
-            float areaUtil;
-
-            Console.Write("Área Útil (m²): ");
-            while (!float.TryParse(Console.ReadLine(), out areaUtil))
-            {
-                Console.Write("Entrada inválida para área útil. Digite novamente: ");
-            }
-            return areaUtil;
-        }
-
-        private string ObterTipoImovel()
-        {
-            _userInteractionHandler.ExibirMensagem("Selecione o Tipo de Imvel:");
-            var listaDeSubTiposImoveis = Enum.GetValues(typeof(SubtipoImovel));
-            int count = 0;
-            foreach (var tipo in listaDeSubTiposImoveis)
-            {
-                _userInteractionHandler.ExibirMensagem($" [{(int)tipo}] {tipo}");
-                count++;
-
-                if (count == 4)
-                {
-                    Console.WriteLine();
-                    count = 0;
-                }
-            }
-
-            return _userInteractionHandler.SolicitarEntrada("\nTipo de Imóvel:", true);
-        }
-
-        private void ConfigurarLocacaoEVenda(Imovel imovel)
-        {
-            Console.Write("O imóvel está disponível para locação? (S/N): ");
-            imovel.ParaLocacao = Console.ReadLine()?.Trim().ToUpper() == "S";
-            if (imovel.ParaLocacao)
-            {
-                Console.Write("Status ATUAL (1) Disponível / (2) Alugado: ");
-                imovel.StatusLocacao = int.TryParse(Console.ReadLine(), out int status) && status == 2 ? "Alugado" : "Disponível";
-
-                Console.Write("Valor do Aluguel: ");
-                if (decimal.TryParse(Console.ReadLine(), out decimal aluguel))
-                {
-                    imovel.ValorAluguel = aluguel;
-                }
-            }
-
-            Console.Write("O imóvel está disponível para venda? (S/N): ");
-            imovel.ParaVenda = Console.ReadLine()?.Trim().ToUpper() == "S";
-            if (imovel.ParaVenda)
-            {
-                Console.Write("Valor de Venda: ");
-                if (decimal.TryParse(Console.ReadLine(), out decimal venda))
-                {
-                    imovel.ValorVenda = venda;
-                }
-            }
-        }
-
-        private Endereco ObterEndereco()
-        {
-            Console.WriteLine("\n==== Informações de Endereço ====");
-            var endereco = new Endereco()
-            {
-                TipoLogradouro = SolicitarTipoLogradouro().ToString(),
-                Logradouro = SolicitarCampo("Logradouro:"),
-                Numero = SolicitarCampo("Número:"),
-                Complemento = SolicitarCampo("complemento:", false),
-                Bairro = SolicitarCampo("Bairro:"),
-                Cidade = "Juiz de Fora", // Pode ser parametrizado ou solicitado
-                UF = "MG", // Pode ser parametrizado ou solicitado
-                CEP = SolicitarCampo("CEP:")
-            };
-
-            Console.WriteLine($"\nEndereço completo: {FormatarEndereco(endereco)}");
-
-            return endereco;
-
-            
-
-            string FormatarEndereco(Endereco endereco)
-            {
-                return $"{endereco.TipoLogradouro} {endereco.Logradouro}, {endereco.Numero} {endereco.Complemento}, " +
-                       $"{endereco.Bairro}, {endereco.Cidade} - {endereco.UF}, CEP: {endereco.CEP}";
-            }
-        }
-
-        private void ConfigurarCaracteristicasInternas(Imovel imovel)
-        {
-            Console.WriteLine("\n==== Configuração de Características Internas ====");
-
-            imovel.Quartos = SolicitarCaracteristicaNumerica("Número de Quartos: ");
-            imovel.Salas = SolicitarCaracteristicaNumerica("Número de Salas: ");
-            imovel.Banheiros = SolicitarCaracteristicaNumerica("Número de Banheiros: ");
-            imovel.Garagens = SolicitarCaracteristicaNumerica("Número de Garagens: ");
-            imovel.Cozinha = SolicitarCaracteristicaBoolean("Possui Cozinha? [1]Sim / [2]Não: ");
-            imovel.Copa = SolicitarCaracteristicaBoolean("Possui Copa? [1]Sim / [2]Não: ");
-            imovel.Quintal = SolicitarCaracteristicaBoolean("Possui Quintal? [1]Sim / [2] Não: ");
-        }
-
-        private int SolicitarCaracteristicaNumerica(string mensagem)
-        {
-            Console.Write(mensagem);
-            return LerIntPositivo();
-        }
-
-        private bool SolicitarCaracteristicaBoolean(string mensagem)
-        {
-            Console.WriteLine(mensagem);
-            return LerOpcaoSimNao();
         }
 
         private void AtribuirProprietarios(Imovel imovel)
@@ -272,73 +138,6 @@ namespace ImobSys.Application.Services
         public List<Imovel> ListarTodosImoveis()
         {
             return _imovelRepository.ListarTodosImovel();
-        }
-
-        public TipoLogradouro SolicitarTipoLogradouro()
-        {
-            Console.WriteLine("Selecione o Tipo de Logradouro:");
-            int count = 0;
-
-            foreach (var tipo in Enum.GetValues(typeof(TipoLogradouro)))
-            {
-                Console.Write($" [{(int)tipo}] {tipo,-8}");
-                count++;
-
-                if (count == 5)
-                {
-                    Console.WriteLine();
-                    count = 0;
-                }
-            }
-
-            Console.Write("Escolha o tipo de Logradouro: ");
-
-            int escolha;
-            while (!int.TryParse(Console.ReadLine(), out escolha) || !Enum.IsDefined(typeof(TipoLogradouro), escolha))
-            {
-                Console.WriteLine("Opção inválida! Digite o número correspondente a uma opção da lista.");
-            }
-
-            return (TipoLogradouro)escolha;
-        }
-
-        private int LerIntPositivo()
-        {
-            while (true)
-            {
-                if (int.TryParse(Console.ReadLine(), out int numero) && numero >= 0)
-                    return numero;
-
-                Console.Write("Entrada inválida! Digite um número positivo: ");
-            }
-        }
-
-        private bool LerOpcaoSimNao()
-        {
-            while (true)
-            {
-                Console.Write("(1) Sim / (2) Não: ");
-                if (int.TryParse(Console.ReadLine(), out int escolha) && (escolha == 1 || escolha == 2))
-                {
-                    return escolha == 1;
-                }
-                Console.WriteLine("Opção inválida! Digite 1 para Sim ou 2 para Não.");
-            }
-        }
-        private string SolicitarCampo(string mensagem, bool obrigatorio = true)
-        {
-            while (true)
-            {
-                Console.Write(mensagem + " ");
-                var entrada = Console.ReadLine();
-
-                if (!obrigatorio || !string.IsNullOrWhiteSpace(entrada))
-                {
-                    return entrada ?? string.Empty;
-                }
-
-                Console.WriteLine("Este campo é obrigatório. Tente novamente.");
-            }
         }
     }
 }
--- Presentation/Handler/UserInteractionHandler.cs
+++ Presentation/Handler/UserInteractionHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ImobSys.Domain;
+using ImobSys.Domain.Entities;
+using ImobSys.Domain.Enums;
+using System;
 
 namespace ImobSys.Presentation.Handler
 {
@@ -76,6 +79,209 @@ namespace ImobSys.Presentation.Handler
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{mensagem}");
             Console.ResetColor();
+        }
+
+        public string ConfigurarTipoImovel()
+        {
+            string tipoImovel = "";
+            do
+            {
+                ExibirMensagem("Tipo do Imóvel \n    (1)Residencial\n    (2)Comercial\n    (3)Misto)\n          Opção: ");
+                if (int.TryParse(Console.ReadLine(), out int escolha))
+                {
+                    tipoImovel = escolha switch
+                    {
+                        1 => "Residencial",
+                        2 => "Comercial",
+                        3 => "Misto",
+                        _ => tipoImovel
+                    };
+                    if (!string.IsNullOrEmpty(tipoImovel)) break;
+                }
+                Console.WriteLine("Opção Inválida! Escolha 1, 2 ou 3.");
+            } while (true);
+            return tipoImovel;
+        }
+
+        public float ObterAreaUtil()
+        {
+            float areaUtil;
+
+            Console.Write("Área Útil (m²): ");
+            while (!float.TryParse(Console.ReadLine(), out areaUtil))
+            {
+                Console.Write("Entrada inválida para área útil. Digite novamente: ");
+            }
+            return areaUtil;
+        }
+
+        public void ConfigurarCaracteristicasInternas(Imovel imovel)
+        {
+            Console.WriteLine("\n==== Configuração de Características Internas ====");
+
+            imovel.Quartos = SolicitarCaracteristicaNumerica("Número de Quartos: ");
+            imovel.Salas = SolicitarCaracteristicaNumerica("Número de Salas: ");
+            imovel.Banheiros = SolicitarCaracteristicaNumerica("Número de Banheiros: ");
+            imovel.Garagens = SolicitarCaracteristicaNumerica("Número de Garagens: ");
+            imovel.Cozinha = SolicitarCaracteristicaBoolean("Possui Cozinha? [1]Sim / [2]Não: ");
+            imovel.Copa = SolicitarCaracteristicaBoolean("Possui Copa? [1]Sim / [2]Não: ");
+            imovel.Quintal = SolicitarCaracteristicaBoolean("Possui Quintal? [1]Sim / [2] Não: ");
+        }
+
+        public Endereco ObterEndereco()
+        {
+            Console.WriteLine("\n==== Informações de Endereço ====");
+            var endereco = new Endereco()
+            {
+                TipoLogradouro = SolicitarTipoLogradouro().ToString(),
+                Logradouro = SolicitarCampo("Logradouro:"),
+                Numero = SolicitarCampo("Número:"),
+                Complemento = SolicitarCampo("complemento:", false),
+                Bairro = SolicitarCampo("Bairro:"),
+                Cidade = "Juiz de Fora", // Pode ser parametrizado ou solicitado
+                UF = "MG", // Pode ser parametrizado ou solicitado
+                CEP = SolicitarCampo("CEP:")
+            };
+
+            Console.WriteLine($"\nEndereço completo: {FormatarEndereco(endereco)}");
+
+            return endereco;
+
+
+
+            string FormatarEndereco(Endereco endereco)
+            {
+                return $"{endereco.TipoLogradouro} {endereco.Logradouro}, {endereco.Numero} {endereco.Complemento}, " +
+                       $"{endereco.Bairro}, {endereco.Cidade} - {endereco.UF}, CEP: {endereco.CEP}";
+            }
+        }
+
+        public string ObterTipoImovel()
+        {
+            Console.WriteLine("Selecione o Tipo de Imvel:");
+
+            var listaDeSubTiposImoveis = Enum.GetValues(typeof(SubtipoImovel));
+            int count = 0;
+
+            foreach (var tipo in listaDeSubTiposImoveis)
+            {
+                Console.Write($" [{(int)tipo}] {tipo}");
+                count++;
+
+                if (count == 4)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
+            }
+
+            return SolicitarEntrada("\nTipo de Imóvel:", true);
+        }
+
+        public TipoLogradouro SolicitarTipoLogradouro()
+        {
+            Console.WriteLine("Selecione o Tipo de Logradouro:");
+            int count = 0;
+
+            foreach (var tipo in Enum.GetValues(typeof(TipoLogradouro)))
+            {
+                Console.Write($" [{(int)tipo}] {tipo,-8}");
+                count++;
+
+                if (count == 5)
+                {
+                    Console.WriteLine();
+                    count = 0;
+                }
+            }
+
+            Console.Write("Escolha o tipo de Logradouro: ");
+
+            int escolha;
+            while (!int.TryParse(Console.ReadLine(), out escolha) || !Enum.IsDefined(typeof(TipoLogradouro), escolha))
+            {
+                Console.WriteLine("Opção inválida! Digite o número correspondente a uma opção da lista.");
+            }
+
+            return (TipoLogradouro)escolha;
+        }
+
+        public void ConfigurarLocacaoEVenda(Imovel imovel)
+        {
+            Console.Write("O imóvel está disponível para locação? (S/N): ");
+            imovel.ParaLocacao = Console.ReadLine()?.Trim().ToUpper() == "S";
+            if (imovel.ParaLocacao)
+            {
+                Console.Write("Status ATUAL (1) Disponível / (2) Alugado: ");
+                imovel.StatusLocacao = int.TryParse(Console.ReadLine(), out int status) && status == 2 ? "Alugado" : "Disponível";
+
+                Console.Write("Valor do Aluguel: ");
+                if (decimal.TryParse(Console.ReadLine(), out decimal aluguel))
+                {
+                    imovel.ValorAluguel = aluguel;
+                }
+            }
+
+            Console.Write("O imóvel está disponível para venda? (S/N): ");
+            imovel.ParaVenda = Console.ReadLine()?.Trim().ToUpper() == "S";
+            if (imovel.ParaVenda)
+            {
+                Console.Write("Valor de Venda: ");
+                if (decimal.TryParse(Console.ReadLine(), out decimal venda))
+                {
+                    imovel.ValorVenda = venda;
+                }
+            }
+        }
+
+        public string SolicitarCampo(string mensagem, bool obrigatorio = true)
+        {
+            while (true)
+            {
+                Console.Write(mensagem + " ");
+                var entrada = Console.ReadLine();
+
+                if (!obrigatorio || !string.IsNullOrWhiteSpace(entrada))
+                {
+                    return entrada ?? string.Empty;
+                }
+
+                Console.WriteLine("Este campo é obrigatório. Tente novamente.");
+            }
+        }
+
+        public bool LerOpcaoSimNao()
+        {
+            while (true)
+            {
+                Console.Write("(1) Sim / (2) Não: ");
+                if (int.TryParse(Console.ReadLine(), out int escolha) && (escolha == 1 || escolha == 2))
+                {
+                    return escolha == 1;
+                }
+                Console.WriteLine("Opção inválida! Digite 1 para Sim ou 2 para Não.");
+            }
+        }
+        private int SolicitarCaracteristicaNumerica(string mensagem)
+        {
+            Console.Write(mensagem);
+            return LerIntPositivo();
+        }
+
+        private bool SolicitarCaracteristicaBoolean(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            return LerOpcaoSimNao();
+        }
+        private int LerIntPositivo()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numero) && numero >= 0)
+                    return numero;
+
+                Console.Write("Entrada inválida! Digite um número positivo: ");
+            }
         }
     }
 }
